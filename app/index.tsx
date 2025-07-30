16624fc7bcd729fad2e42ccc3f8c29411505c4d9
nawaf <nawafalrenezi@gmail.com>
2025-07-30 20:49:09 +0300

    still in progress

---
+++ app/index.tsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import { Button, Image, Text, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+
+export default function Index() {
+  const [count, SetCount] = useState(0);
+  const [count1, SetCount1] = useState(0);
+  const [boosted, setBoosted] = useState(false);
+
+  const increment = () => {
+    const value = boosted ? 3 : 1;
+    SetCount(count + value);
+    SetCount1(count1 + value);
+  };
+
+  const enableBoost = () => {
+    setBoosted(true);
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#271c5fff",
+      }}
+    >
+      <ScrollView>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Image
+            source={{
+              uri: "https://the-death-star.netlify.app/static/media/the-death-star.ed327467.png",
+            }}
+            style={{
+              width: 200,
+              height: 200,
+              marginRight: 400,
+            }}
+          />
+          <View style={{ display: "flex", flexDirection: "row" }}>
+            <Image
+              source={{
+                uri: "https://static.vecteezy.com/system/resources/thumbnails/020/716/945/small_2x/3d-glossy-dollar-coin-golden-reflective-dollar-coin-3d-illustration-png.png",
+              }}
+              style={{ width: 100, height: 100, marginLeft: 200 }}
+            />
+            <View style={{ marginTop: 40 }}>
+              <Text style={{ fontSize: 50, color: "white" }}>X {count}</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={{ marginTop: 50, alignItems: "center" }}>
+          {!boosted && (
+            <View style={{ marginTop: 30, alignItems: "center" }}>
+              <Button
+                title="Get Energy!!!"
+                onPress={enableBoost}
+                color="#00ffcc"
+              />
+            </View>
+          )}
+          <Text style={{ color: "white", fontSize: 150 }}>{count1}</Text>
+          <Text style={{ fontSize: 20, color: "white" }}>
+            Planets Destroyed
+          </Text>
+          <Button
+            title="Destroy a Planet!!"
+            onPress={increment}
+            color="#f30606ff"
+          />
+          <Text style={{ color: "white", opacity: 0.5 }}>
+            {boosted ? "3 Planets per click (Boosted!)" : "1 Planet per click"}
+          </Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
