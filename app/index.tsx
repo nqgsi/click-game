4a689295fa5f676e98fc935cbe6730ce7173d651
nawaf <nawafalrenezi@gmail.com>
2025-07-31 16:51:56 +0300

    click-game done

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Button, Image, Text, View } from "react-native";
+import * as Animatable from "react-native-animatable";
 import { ScrollView } from "react-native-gesture-handler";
 
 export default function Index() {
@@ -45,42 +46,81 @@ export default function Index() {
             }}
           />
           <View style={{ display: "flex", flexDirection: "row" }}>
-            <Image
+            <Animatable.Image
+              animation="bounce"
+              iterationCount="infinite"
               source={{
                 uri: "https://static.vecteezy.com/system/resources/thumbnails/020/716/945/small_2x/3d-glossy-dollar-coin-golden-reflective-dollar-coin-3d-illustration-png.png",
               }}
-              style={{ width: 100, height: 100, marginLeft: 200 }}
+              style={{
+                width: 100,
+                height: 100,
+                marginLeft: 200,
+                marginTop: 150,
+              }}
             />
-            <View style={{ marginTop: 40 }}>
-              <Text style={{ fontSize: 50, color: "white" }}>X {count}</Text>
-            </View>
+
+            <Animatable.Text animation="bounce" iterationCount="infinite">
+              <View style={{ marginTop: 150 }}>
+                <Text style={{ fontSize: 50, color: "white" }}>X {count}</Text>
+              </View>
+            </Animatable.Text>
           </View>
         </View>
-
         <View style={{ marginTop: 50, alignItems: "center" }}>
           {!boosted && (
-            <View style={{ marginTop: 30, alignItems: "center" }}>
-              <Button
-                title="Get Energy!!!"
-                onPress={enableBoost}
-                color="#00ffcc"
-              />
-            </View>
+            <Animatable.View animation="flash" iterationCount="infinite">
+              <View style={{ marginTop: 30, alignItems: "center" }}>
+                <Button
+                  title="Get Energy!!!"
+                  onPress={enableBoost}
+                  color="#00ffcc"
+                />
+              </View>
+            </Animatable.View>
           )}
+
           <Text style={{ color: "white", fontSize: 150 }}>{count1}</Text>
           <Text style={{ fontSize: 20, color: "white" }}>
             Planets Destroyed
           </Text>
-          <Button
-            title="Destroy a Planet!!"
-            onPress={increment}
-            color="#f30606ff"
-          />
+
+          <Animatable.View animation="pulse" iterationCount="infinite">
+            <Button
+              title="Destroy a Planet!!"
+              onPress={increment}
+              color="#f30606ff"
+            />
+          </Animatable.View>
           <Text style={{ color: "white", opacity: 0.5 }}>
             {boosted ? "3 Planets per click (Boosted!)" : "1 Planet per click"}
           </Text>
         </View>
       </ScrollView>
+      {count1 >= 50 && (
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgba(0, 0, 0, 0.8)",
+          }}
+        >
+          <Image
+            source={{
+              uri: "https://media.tenor.com/mZZoOtDcouoAAAAM/stop-it-get-some-help.gif",
+            }}
+            style={{
+              width: 300,
+              height: 300,
+            }}
+          />
+        </View>
+      )}
     </View>
   );
 }
